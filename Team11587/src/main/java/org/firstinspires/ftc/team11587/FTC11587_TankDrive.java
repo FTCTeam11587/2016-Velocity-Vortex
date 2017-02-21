a8a01fa4369e23b5359df869c9b79a87fb0e3884
hitchcme <hitchcme@gmail.com>
2017-02-20 20:26:04 -0700

    Latest changes to FTC11587_TankDrive.java

--- Team11587/src/main/java/org/firstinspires/ftc/team11587/FTC11587_TankDrive.java
+++ Team11587/src/main/java/org/firstinspires/ftc/team11587/FTC11587_TankDrive.java
@@ -20,15 +20,14 @@ public class FTC11587_TankDrive extends LinearOpMode {
 
         //Define hardware for task functions//
         DcMotor         lift_motor;
-        Servo           left_grapple_servo;
-        Servo           right_grapple_servo;
-        DigitalChannel  upper_limit_switch;
-        DigitalChannel  lower_limit_switch;
+        Servo           grapple_servo;
+        //DigitalChannel  upper_limit_switch;
+        //DigitalChannel  lower_limit_switch;
 
         //Define variables for task functions//
-        double          lift_motor_power = 0.1;
-        double          leftGrapplePosition = 1.0;      //Grapple initializes to closed//
-        double          rightGrapplePosition = 1.0;     //Grapple initializes to closed//
+        double          lift_motor_power = 0.8;
+        double          GrapplePosition = 1.0;      //Grapple initializes to closed//
+        //double          rightGrapplePosition = 1.0;     //Grapple initializes to closed//
         final double    GRAPPLE_SPEED = 0.02;           //Grapple actuation speed//
 
         HardwarePushbot robot = new HardwarePushbot();
@@ -43,13 +42,15 @@ public class FTC11587_TankDrive extends LinearOpMode {
 
             //Initialize aux hardware//
             lift_motor = hardwareMap.dcMotor.get("lift_motor");		// build config profile
-            left_grapple_servo = hardwareMap.servo.get("left_grapple");	// build config profile
-            right_grapple_servo = hardwareMap.servo.get("right_grapple");	// build config profile
-            left_grapple_servo.setPosition(leftGrapplePosition);
-            right_grapple_servo.setPosition(rightGrapplePosition);
+            grapple_servo = hardwareMap.servo.get("grapple");	// build config profile
+            //right_grapple_servo = hardwareMap.servo.get("right_grapple");	// build config profile
+            grapple_servo.setPosition(leftGrapplePosition);
+            //right_grapple_servo.setPosition(rightGrapplePosition);
+			int WE_WERE_CONTROLLING_LIFT = 0;
+			int WWNCLCNT = 0;
 
-            upper_limit_switch = hardwareMap.digitalChannel.get("upperLimitSwitch");	// build config profile
-            lower_limit_switch = hardwareMap.digitalChannel.get("lowerLimitSwitch");	// build config profile
+            //upper_limit_switch = hardwareMap.digitalChannel.get("upperLimitSwitch");	// build config profile
+            //lower_limit_switch = hardwareMap.digitalChannel.get("lowerLimitSwitch");	// build config profile
 
             telemetry.addData("Say: ", "Hello Knight!");
             telemetry.update();
@@ -70,49 +71,68 @@ public class FTC11587_TankDrive extends LinearOpMode {
                     left /= max;
                     right /= max;
                 }
+                if (gamepad1.left_trigger > 0.3) {
+                    robot.leftMotor.setPower( left/2 );
+                    robot.rightMotor.setPower( right/2 );
+                }
+                else if (gamepad1.left_trigger < 0.3 ) {
+                    robot.leftMotor.setPower( (left/3) * 1.2);
+                    robot.rightMotor.setPower( (right/3) * 1.2);
+                }
 
-                robot.leftMotor.setPower(left);
-                robot.rightMotor.setPower(right);
+
 
             /*Code to open/close Cap Ball grapple servo*/
 
             //Use Gamepad 1 D-pad L/R to actuate the grapple servo//
             if (gamepad1.dpad_right) {
-                leftGrapplePosition += GRAPPLE_SPEED;
-                rightGrapplePosition -= GRAPPLE_SPEED;
+                GrapplePosition += GRAPPLE_SPEED;
+                //rightGrapplePosition -= GRAPPLE_SPEED;
             }
             else if (gamepad1.dpad_left) {
-                leftGrapplePosition -= GRAPPLE_SPEED;
-                rightGrapplePosition += GRAPPLE_SPEED;
+                GrapplePosition -= GRAPPLE_SPEED;
+                //rightGrapplePosition += GRAPPLE_SPEED;
             }
 
-            leftGrapplePosition = Range.clip(leftGrapplePosition,0.0,1.0);
-				left_grapple_servo.setPosition(leftGrapplePosition);
+            GrapplePosition = Range.clip(GrapplePosition,0.0,1.0);
+				grapple_servo.setPosition(GrapplePosition);
 
-            rightGrapplePosition = Range.clip(rightGrapplePosition,0.0,1.0);
-                right_grapple_servo.setPosition(-rightGrapplePosition);
-                telemetry.addData("Left Grapple Position: ", "%.2f",leftGrapplePosition);
-                telemetry.addData("Right Grapple Position:", "%.2f",rightGrapplePosition);
+            //rightGrapplePosition = Range.clip(rightGrapplePosition,0.0,1.0);
+               // right_grapple_servo.setPosition(-rightGrapplePosition);
+                telemetry.addData("Grapple Position: ", "%.2f",GrapplePosition);
+                //telemetry.addData("Right Grapple Position:", "%.2f",rightGrapplePosition);
                 telemetry.update();
 
             //Use Gamepad 1 D-pad Up/Down to actuate the lift motor//
-            boolean uLim = upper_limit_switch.getState();
-            boolean lLim = lower_limit_switch.getState();
+            //boolean uLim = upper_limit_switch.getState();
+            //boolean lLim = lower_limit_switch.getState();
 
-            if (gamepad1.dpad_up)
-            //    if (uLim = false)  //Check this...generates caution...always true?
-                    lift_motor.setPower(lift_motor_power);
-            //    else if (uLim = true)
-            //        lift_motor.setPower(0.0);
 
-            if (gamepad1.dpad_down)
+            if (gamepad1.dpad_up == true) {
+				//    if (uLim = false)  //Check this...generates caution...always true?
+				lift_motor.setPower(lift_motor_power);
+				WE_WERE_CONTROLLING_LIFT = 1;
+				WWNCLCNT = 0;
+				//    else if (uLim = true)
+				//        lift_motor.setPower(0.0);
+			}
+            else if (gamepad1.dpad_down == true)
             //    if (lLim = false)
-                    lift_motor.setPower(-lift_motor_power);
+				lift_motor.setPower(-lift_motor_power);
+				WE_WERE_CONTROLLING_LIFT = 1;
+				WWNCLCNT = 0;
             //    else if (lLim = false)
             //        lift_motor.setPower(0.0);
-
-
-                 robot.waitForTick(40);
+			else if (gamepad1.dpad_up == false && gamepad1.dpad_down == false && WE_WERE_CONTROLLING_LIFT == 1 && WWNCLCNT < 300) {
+				lift_motor.setPower(.05);
+				WWNCLCNT++;
+			}
+			else if (gamepad1.dpad_up == false && gamepad1.dpad_down == false && WWNCLCNT == 300) {
+				lift_motor.setPower(0);
+				WE_WERE_CONTROLLING_LIFT = 0;
+				//WWNCLCNT = 300;
+			}
+            	robot.waitForTick(40);
             }
         }
     }
