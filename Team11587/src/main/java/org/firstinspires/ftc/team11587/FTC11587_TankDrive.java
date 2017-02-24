d9ab386e19ce296c1168a7f0a1f0fbb44d52f1e3
Kurt Kochendarfer <KE7KUS@users.noreply.github.com>
2017-02-24 20:25:54 +0000

    Added slow drive + invert Y-axis code
    Adds improved UI functionality for driving while carrying the Cap Ball.  Holding left trigger on the controller cuts drive power by 75% reducing the tendency to over-control the robot.  Also inverts Y-axis inputs while holding left trigger to make Y-axis work the same while carrying Cap Ball as it does for normal driving.

--- Team11587/src/main/java/org/firstinspires/ftc/team11587/FTC11587_TankDrive.java
+++ Team11587/src/main/java/org/firstinspires/ftc/team11587/FTC11587_TankDrive.java
@@ -22,15 +22,10 @@ public class FTC11587_TankDrive extends LinearOpMode {
         DcMotor         lift_motor;
         Servo           grapple_servo;
 
-        //DigitalChannel  upper_limit_switch;
-        //DigitalChannel  lower_limit_switch;
-
         //Define variables for task functions//
         double          lift_motor_power = 0.8;
         double          GrapplePosition = 1.0;      //Grapple initializes to closed//
-        //double          rightGrapplePosition = 1.0;     //Grapple initializes to closed//
-    
-        final double    GRAPPLE_SPEED = 0.02;           //Grapple actuation speed//
+        final double    GRAPPLE_SPEED = 0.02;       //Grapple actuation speed//
 
         HardwarePushbot robot = new HardwarePushbot();
 
@@ -46,56 +41,54 @@ public class FTC11587_TankDrive extends LinearOpMode {
 
             lift_motor = hardwareMap.dcMotor.get("lift_motor");		// build config profile
             grapple_servo = hardwareMap.servo.get("grapple_servo");	// build config profile
-            //right_grapple_servo = hardwareMap.servo.get("right_grapple");	// build config profile
             grapple_servo.setPosition(GrapplePosition);
-            //right_grapple_servo.setPosition(rightGrapplePosition);
-			int WE_WERE_CONTROLLING_LIFT = 0;
-			int WWNCLCNT = 0;
+	        int WE_WERE_CONTROLLING_LIFT = 0;
+		int WWNCLCNT = 0;
 
-            //upper_limit_switch = hardwareMap.digitalChannel.get("upperLimitSwitch");	// build config profile
-            //lower_limit_switch = hardwareMap.digitalChannel.get("lowerLimitSwitch");	// build config profile
-            //grapple_servo = hardwareMap.servo.get("grapple_servo");
-            //grapple_servo.setPosition(GrapplePosition);
-
-
-            telemetry.addData("Say: ", "Hello Knight!");
+            telemetry.addData("Say: ", "Hello Starry Knight!");
             telemetry.update();
 
             waitForStart();
 
             while (opModeIsActive()) {
 
-                left = -gamepad1.left_stick_y + gamepad1.right_stick_x;
+               public static limit_max() {
+                   max = Math.max(Math.abs(left), Math.abs(right));
+		   if (max > 1.0) {
+                       left /= max;
+                       right /= max;
+                   }
+	       }
+
+	       public static motor_data() {
+		    telemetry.addData("Left Trigger: ", "%.2f", gamepad1.left_trigger);
+	            telemetry.addData("Left Motor: ", left);
+	            telemetry.addData("Right Motor: ", right);
+	            telemetry.addData("Max Motor: ", max);
+		    telemetry.update();
+	       }
+
+	    //Code to enable Cap Ball Drive - cuts drive power by 75% and inverts the gamepad1 stick Y-axis//
+
+            if (gamepad1.left_trigger > 0.3) {
+                robot.leftMotor.setPower( left/4 );
+                robot.rightMotor.setPower( right/4 );
+		left = gamepad1.left_stick_y + gamepad1.right_stick_x;
+		right = gamepad1.right_stick_y - gamepad1.right_stick_x;
+		limit_max();
+
+		telemetry.addData("Op Mode: ","Cap Ball Drive");
+		motor_data();
+                }
+            else {
+                robot.leftMotor.setPower( (left/3) * 1.2);
+                robot.rightMotor.setPower( (right/3) * 1.2);
+	 	left = -gamepad1.left_stick_y + gamepad1.right_stick_x;
                 right = -gamepad1.left_stick_y - gamepad1.right_stick_x;
-                max = Math.max(Math.abs(left), Math.abs(right));
-                telemetry.addData("Left Motor Pos: ", left);
-                telemetry.addData("Right Motor Pos: ", right);
-                telemetry.addData("Max Motor Pos: ", max);
-                telemetry.update();
+		limit_max();
 
-                if (max > 1.0) {
-                    left /= max;
-                    right /= max;
-                }
-                if (gamepad1.left_trigger > 0.3) {
-                    robot.leftMotor.setPower( left/2 );
-                    robot.rightMotor.setPower( right/2 );
-                }
-                else if (gamepad1.left_trigger < 0.3 ) {
-                    robot.leftMotor.setPower( (left/3) * 1.2);
-                    robot.rightMotor.setPower( (right/3) * 1.2);
-                }
-
-
-
-                if (gamepad1.left_trigger > 0) {
-                    robot.leftMotor.setPower(left * .5);
-                    robot.rightMotor.setPower(right * .5);
-                }
-
-                else if (gamepad1.left_trigger == 0) {
-                    robot.leftMotor.setPower(left);
-                    robot.rightMotor.setPower(right);
+		telemetry.addData("Op Mode: ","Normal Drive");
+		motor_data();
                 }
 
             /*Code to open/close Cap Ball grapple servo*/
@@ -103,55 +96,42 @@ public class FTC11587_TankDrive extends LinearOpMode {
             //Use Gamepad 1 D-pad L/R to actuate the grapple servo//
             if (gamepad1.dpad_right) {
                 GrapplePosition += GRAPPLE_SPEED;
-				//rightGrapplePosition -= GRAPPLE_SPEED;
-            }
             else if (gamepad1.dpad_left) {
                 GrapplePosition -= GRAPPLE_SPEED;
-                //rightGrapplePosition += GRAPPLE_SPEED;
-            }
-
+             }
             GrapplePosition = Range.clip(GrapplePosition,0.0,1.0);
-				grapple_servo.setPosition(GrapplePosition);
+		grapple_servo.setPosition(GrapplePosition);
 
-            //rightGrapplePosition = Range.clip(rightGrapplePosition,0.0,1.0);
-            // right_grapple_servo.setPosition(-rightGrapplePosition);
             telemetry.addData("Grapple Position: ", "%.2f",GrapplePosition);
-            //telemetry.addData("Right Grapple Position:", "%.2f",rightGrapplePosition);
             telemetry.update();
 
-            //Use Gamepad 1 D-pad Up/Down to actuate the lift motor//
-            //boolean uLim = upper_limit_switch.getState();
-            //boolean lLim = lower_limit_switch.getState();
-
+	    //Code to control pulley lift//
 
             if (gamepad1.dpad_up == true) {
-				//    if (uLim = false)  //Check this...generates caution...always true?
-				lift_motor.setPower(lift_motor_power);
-				WE_WERE_CONTROLLING_LIFT = 1;
-				WWNCLCNT = 0;
-				//    else if (uLim = true)
-				//        lift_motor.setPower(0.0);
-			}
-            else if (gamepad1.dpad_down == true) {
-				//    if (lLim = false)
-				lift_motor.setPower(-lift_motor_power);
-				WE_WERE_CONTROLLING_LIFT = 1;
-				WWNCLCNT = 0;
-				//    else if (lLim = false)
-				//        lift_motor.setPower(0.0);
-			}
-			else if (gamepad1.dpad_up == false && gamepad1.dpad_down == false && WE_WERE_CONTROLLING_LIFT == 1 && WWNCLCNT < 300) {
-				lift_motor.setPower(.05);
-				WWNCLCNT++;
-			}
-			else if (gamepad1.dpad_up == false && gamepad1.dpad_down == false && WWNCLCNT == 300) {
-				lift_motor.setPower(0);
-				WE_WERE_CONTROLLING_LIFT = 0;
-				//WWNCLCNT = 300;
-			}
-            robot.waitForTick(40);
+		lift_motor.setPower(lift_motor_power);
+		WE_WERE_CONTROLLING_LIFT = 1;
+		WWNCLCNT = 0;
+	    }
 
+            else if (gamepad1.dpad_down == true) {
+		lift_motor.setPower(-lift_motor_power);
+		WE_WERE_CONTROLLING_LIFT = 1;
+		WWNCLCNT = 0;
+	    }
+		
+	    else if (gamepad1.dpad_up == false && gamepad1.dpad_down == false && WE_WERE_CONTROLLING_LIFT == 1 && WWNCLCNT < 300) {
+		lift_motor.setPower(.01);
+		WWNCLCNT++;
+	    }
+		
+	    else if (gamepad1.dpad_up == false && gamepad1.dpad_down == false && WWNCLCNT == 300) {
+		lift_motor.setPower(0);
+		WE_WERE_CONTROLLING_LIFT = 0;
+	    }
+            
+	    robot.waitForTick(40);
             }
         }
     }
+
 
